b74fbfd949945963b9a8a2b7c4edc9e81584a778
Simon Asika <asika32764@gmail.com>
2014-08-18 00:25:13 +0800

    Copy url & id handler

--- src/Minurl/Helper/Base62Helper.php
+++ src/Minurl/Helper/Base62Helper.php
@@ -16,6 +16,20 @@ namespace Minurl\Helper;
 class Base62Helper
 {
 	/**
+	 * Property hash.
+	 *
+	 * @var  array
+	 */
+	protected static $hash = array(9, 5, 1, 3, 7, 4, 6, 2, 8, 0);
+
+	/**
+	 * Property offset.
+	 *
+	 * @var  int
+	 */
+	protected static $offset = 12345;
+
+	/**
 	 * encode
 	 *
 	 * @param int $int
@@ -24,7 +38,7 @@ class Base62Helper
 	 */
 	public static function encode($int)
 	{
-		return Base62::convert($int, 10, 62);
+		return Base62::convert(static::obfuscate($int), 10, 62);
 	}
 
 	/**
@@ -36,7 +50,47 @@ class Base62Helper
 	 */
 	public static function decode($int)
 	{
-		return Base62::convert($int, 62, 10);
+		return static::unObfuscate(Base62::convert($int, 62, 10));
+	}
+
+	/**
+	 * obfuscate
+	 *
+	 * @param string $int
+	 *
+	 * @return  int
+	 */
+	public static function obfuscate($int)
+	{
+		$ints = str_split((string) (int) $int);
+
+		foreach ($ints as &$int)
+		{
+			$int = static::$hash[$int];
+		}
+
+		return (int) implode('', $ints) + static::$offset;
+	}
+
+	/**
+	 * unObfuscate
+	 *
+	 * @param string $int
+	 *
+	 * @return  int
+	 */
+	public static function unObfuscate($int)
+	{
+		$int = ((int) $int) - static::$offset;
+
+		$ints = str_split((string) $int);
+
+		foreach ($ints as &$int)
+		{
+			$int = array_search($int, static::$hash);
+		}
+
+		return (int) implode('', $ints);
 	}
 }
  
